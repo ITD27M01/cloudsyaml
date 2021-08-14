67576caab6dd067d7a85ba87544eb6bc29f41df3
Igor Tiunov <igortiunov@gmail.com>
2021-08-14 20:40:13 +0300

    Fix test for filtering

--- tests/test_list.py
+++ tests/test_list.py
@@ -3,7 +3,7 @@ from clouds import listing
 
 loader = cloud_config.OpenStackConfig(config_files=['tests/clouds.yaml'])
 CLOUDS = loader.get_all_clouds()
-CLOUD1 = [loader.get_one_cloud(cloud='cloud1')]
+CLOUD1 = loader.get_one_cloud(cloud='cloud1')
 
 
 class Args:
@@ -26,4 +26,4 @@ def test_listing_filtered(mocker):
     mocker.patch.object(listing, 'get_clouds')
     listing.get_clouds.return_value = CLOUDS
     args.grep = ['cloud1']
-    assert listing.list_clouds(args) == CLOUD1
+    assert CLOUD1 in listing.list_clouds(args)
